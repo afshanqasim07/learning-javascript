5cc7d3449873235fdcbd1d2f7bc168e728f496d7
afshanqasim07 <afshaanqasim07@gmail.com>
2024-07-23 11:55:52 -0700

    learned variables and datatypes

---
+++ variable and data types/app.js
@@ -0,0 +1,66 @@
+// day 1 of learning js 
+
+
+// alert 
+
+alert("hello welcome back ");
+
+
+// how to print anything 
+
+console.log("we are learning javascript");
+
+
+// variables started
+// 1.   var we dont use it anymore global  
+var username = "afshanqasim" ;
+console.log(username);
+
+// 2. let its block and can be updated.
+
+let password = "afshii123 ";
+password = "afshanqasim07";
+console.log(password);
+// const is un update able and redeclared its fixed.
+const age = 19;
+console.log(age);
+
+// variables ended 
+
+
+// data types
+
+number = 1;
+string = "string";
+boolean = true /false ;
+let x= undefined ;
+let y = null ;
+
+// object 
+   
+
+// make an amazon product data base object to store its data
+
+
+const product = {
+    tittle : "ballpen",
+    ratings: "4stars",
+    price : "50$" ,
+    offer : "5%off" ,
+    deal : "true",
+}
+console.log(product);
+typeof product;
+
+// make a data base for an instagram profile 
+
+
+const profile = {
+    username : "afshanqasim07",
+    followers : "50k" ,
+    following : 50 ,
+    bio : "learning javascript" ,
+    isfollow : "True",
+    }
+
+    console.log(profile);
