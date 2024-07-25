a8aec529dbc26bcefef6c7a49518035ac54fccd5
afshanqasim07 <afshaanqasim07@gmail.com>
2024-07-25 10:11:00 -0700

    operators done

---
+++ operators/app.js
@@ -0,0 +1,45 @@
+// operators
+
+ 
+var a = 5;
+var b = 4;
+
+// airthmatic operators
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a--);
+
+
+
+// assigment operators
+   console.log(a+=3);
+   console.log(b-=2);
+   console.log(a*=3);
+   console.log(b/=8);
+   console.log(a**=b);
+
+//    comparision operators
+ let x = 30;
+ let y = "30";
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x!==y);
+console.log(x>y);
+console.log(x>=y);
+console.log(x<y);
+console.log(x<=y);
+
+
+// logical operators && and ||
+
+
+
+ console.log(
+        (15 % 4 === 3 && 20 > 15 || 7 * 3 === 21 && 12 - 6 !== 6) &&  
+      (8 % 3 === 2 || 25 <= 30 && 5 !== 5) ||
+      (100 / 10 === 10 && 9 == "9" || 14 + 1 !== 15 && 7 <= 7) );
