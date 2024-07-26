0a7ac724e9f02a0aa8ca3f6b5629aaf2cf13a029
afshanqasim07 <afshaanqasim07@gmail.com>
2024-07-26 04:30:24 -0700

    "Added and commented out code for 9 conditional statement questions, including checking multiples of 5, grading students, divisibility by 3, even/odd numbers, age checks, and ticket price calculations."

--- conditional statements/app.js
+++ conditional statements/app.js
@@ -3,34 +3,152 @@
 // question 1
 
 // get user to input a number using prompt check if the number is multiple of 5 or not ?
-let num = prompt("enter the number");
-console.log(num)
-if (num % 5 ===0){
-    console.log("its a multiple of 5");
-}
-else{
-    console.log("its not a multiple of 5");
-}
+
+// answer 1
+// let num = prompt("enter the number");
+// console.log(num)
+// if (num % 5 ===0){
+//     console.log("its a multiple of 5");
+// }
+// else{
+//     console.log("its not a multiple of 5");
+// }
 // question 2
 // write a code which can give grades to student according to their scores
 // 90 to 100 will be A ,  70 to 89 will be B , 60 to 69 will be C , 50 to 59 will be D , 49 to 0 will be F
 
-let marks = prompt("enter your marks");
-console.log(marks);
+
+// answer 2
+// let marks = prompt("enter your marks");
+// console.log(marks);
  
 
-if (marks>=90 && marks <=100){
-    console.log("you got A ");
-}
-else if (marks >=70 && marks <=89){
-    console.log("you got B ");
-}
-else if (marks >=60 && marks <=69){
-    console.log("you got C ");
-}
-else if (marks >=50 && marks <=59){
-    console.log("you got D ");
-}
-else {
-    console.log("you are fail ,better luck next time");
-}
+// if (marks>=90 && marks <=100){
+//     console.log("you got A ");
+// }
+// else if (marks >=70 && marks <=89){
+//     console.log("you got B ");
+// }
+// else if (marks >=60 && marks <=69){
+//     console.log("you got C ");
+// }
+// else if (marks >=50 && marks <=59){
+//     console.log("you got D ");
+// }
+// else {
+//     console.log("you are fail ,better luck next time");
+// }
+
+// question 3
+// . Write a program to check whether the given input number is divisible by 3 or else show a message “Number is not divisible
+// by 3”.
+ 
+
+// answer 3
+// let value = prompt("enter the number");
+// console.log(value);
+// if (value % 3===0){
+//     console.log("number is divisible by 3") ;
+// }
+// else {
+//     console.log("its not divisible by 3");
+// }
+
+// question 4
+// Write a program that checks whether the given input is an even number or an odd number?
+
+// answer 4
+// let num = prompt("enter the number");
+// console.log(num);
+
+// if (num % 2 ===0 ){
+//     console.log("the given number is even");
+// }
+// else {
+//     console.log("number is odd");
+// }
+
+// question 5
+
+// Write an if/else statement with the following condition: If the variable age is greater than 18, output "Old enough",
+// otherwise output "Too young".
+
+// answer 5
+// let age = prompt("Enter your age");
+// console.log(age);
+
+// if (age >=18){
+//     console.log("Old enough");
+// }
+// else {
+//     console.log("Too young")
+// }
+
+// question 6 
+// Write a program that prompts the user for their name, and then displays a special greeting to that person if their name is the
+// same as yours. If the name entered by the user is anything other than your name, your code should not produce any  output
+
+
+// answer 6 
+
+// let myname = "ayeza";
+// console.log(myname);
+
+// let username = prompt("enter your username");
+// if (username === myname){
+//     console.log("hello" +  username + ", welcome back ayeza");
+// }
+
+
+// question n 7 
+
+// Scenario: Write a program that determines whether a given number is positive or not?
+
+// let num = prompt("enter the number");
+// console.log(num);
+
+// if (num>0){
+//     console.log("number is positive");
+// }
+// else {
+//     console.log("number is negative")
+// }
+
+
+// question n 8
+
+// Scenario: Write a program to determine the greater of two numbers.
+
+// Solution:
+
+// let sum1 = 20;
+// let sum2 = 40;
+
+
+// if (sum1>sum2){
+//     console.log("sum1 is greater than sum2");
+// }
+// else {
+//     console.log("sum2 is greater");
+// }
+
+
+// question no 9
+
+// Write a program that calculates the ticket price based on age.?
+
+ 
+// let userage = +prompt("enter you age");
+
+// if (userage===10 || userage <=10){
+//     console.log("ticketprice = 50");
+// }
+// else if (userage=== 15 || userage <=15){
+//     console.log("ticket price is 100");
+// }
+// else if (userage=== 18 || userage <=18){
+//     console.log("ticket price is 150");
+// }
+// else  {
+//     console.log("ticketprice is for 300pkr.")
+// }
