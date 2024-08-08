6039fa1bc9f3dddec5e084b4f48125a6332b7a39
usernmae <afshaanqasim07@gmail.com>
2024-08-08 10:02:56 -0700

    "Added new for loop, do-while loop, and while loop examples; modified existing while loop condition; added practice questions for printing even numbers and multiples of 3; and created a simple number guessing game."

---
+++ excercise of if else and loops/app.js
@@ -0,0 +1,57 @@
+// if else excersise
+
+console.log("hello world ");
+
+// Write a program to check whether the given input number is
+// divisible by 3 or else show a message “Number is not divisible
+// by 3”.
+
+// solution
+// let num = prompt("enter the number");
+// if (num % 3 === 0) {
+//     console.log("number is divisible by 3");
+// }
+// else {
+//     console.log("number is not divisible by 3")
+// }
+  
+
+// Write a program that checks whether the given input is an even
+// number or an odd number
+// let evenno = +prompt("enter the even number");
+// if (evenno % 2 === 0) {
+//     console.log("number is even");
+// }
+// else {
+//     console.log("number is odd");
+// }
+
+// Write an if/else statement with the following condition:
+// If the variable age is greater than 18, output "Old enough",
+// otherwise output "Too young"
+
+
+// let age = +prompt("enter your age")
+
+// if (age >= 18) {
+//     console.log("Old enough");
+// }
+// else {
+//     console.log("too young");
+// }
+
+// Write a program that prompts the user for their name, and then
+// displays a special greeting to that person if their name is the
+// same as yours. If the name entered by the user is anything
+// other than your name, then given name is not correct
+
+// let varname = "pareshay";
+// let username = prompt("Enter the name");
+
+// if (username === varname) {
+//     alert("welcome back pareshay");
+// }
+// else {
+//     console.log("given name is not correct");
+// }
+
--- loops/script.js
+++ loops/script.js
@@ -11,12 +11,52 @@ for (let i = 0;i <5;i++) {
 let sum = 0;
 
 for (let num =1;num<=10;num++ ){
-    sum = sum +num;
+    sum = sum +num; 
+    
 }
 console.log(sum);
 
 let age= 0;
-while (age<=18){
+while (age<18){
     console.log("you can drive")
     age++;
 }
+
+let i = 0;
+do {
+    console.log("afshanqasim");
+    i++;
+}while (i<=20)
+
+
+
+    // practice questions
+
+    // print all even numbers from 0 to 100 
+
+for (i = 0 ; i<=100 ;i++){
+    if (i%2 ===0){
+        console.log(i);
+    }
+}
+
+  let j = 3;
+
+  while (j<=99){
+    if (j%3===0){
+        console.log(j);
+        
+    }
+    j++;
+  }
+// create game where user will guess the correct game no ask user to keep guesing untill the user guess the right number
+
+let gamenum = 20;
+
+let usernum = prompt+("Guess the game number");
+console.log(usernum);
+
+while (gamenum!== usernum){
+    let usernum = prompt+("you guess the wrong number, guess the right number");
+}
+console.log("congratulations you guess the right number");
