2e3f95bb6875a1d49128f0dc46602cc8f9842d58
usernmae <afshaanqasim07@gmail.com>
2024-08-22 04:13:59 -0700

    date and time

---
+++ date and time/app.js
@@ -0,0 +1,18 @@
+// date and time
+
+let mydate = new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toLocaleString());
+console.log(mydate.getFullYear());
+console.log(mydate.getDay());
+console.log(mydate.getMonth());
+console.log(mydate.toDateString());
+console.log(typeof (mydate.getHours()));
+console.log(mydate.getSeconds());
+console.log(mydate.getHours());
+console.log(mydate.getMilliseconds());
+console.log(mydate.getSeconds());
+console.log(mydate.getMonth());
+console.log(mydate.getMinutes());
+console.log(mydate.getTime());
