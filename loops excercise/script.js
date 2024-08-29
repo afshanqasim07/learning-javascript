169f7fa0797e6f5354ec1a49ff8730dc8166eeef
usernmae <afshaanqasim07@gmail.com>
2024-08-29 04:42:55 -0700

    sums

--- loops excercise/script.js
+++ loops excercise/script.js
@@ -57,15 +57,69 @@ let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
 // . You have an array A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
 // Write a program to enable “search by user input” in an array.After searching, prompt the user whether the given item is found in the list or not. Example:
 
-let bakeryitems = ["cake", "apple pie", "cookie", "chips", "patties"];
-let item= prompt("enter the item you want");
-for (i = 0; i < bakeryitems.length; i++){
-    if (item === bakeryitems[i]) {
-        console.log(`${item} found in the list`)
-        break
-    } else {
-        console.log(` we are sorry ${item} is not available in our bakery`);
-    }
-}
-// Write a program to identify the largest number in the givenarray. A = [24, 53, 78, 91, 12]
-// solution
+// let bakeryitems = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let item= prompt("enter the item you want");
+// for (i = 0; i < bakeryitems.length; i++){
+//     if (item === bakeryitems[i]) {
+//         console.log(`${item} found in the list`)
+//         break
+//     } else {
+//         console.log(` we are sorry ${item} is not available in our bakery`);
+//     }
+// }
+// Write a program to print multiples of 5 ranging 1 to 100
+// let num = 5;
+// let length = 20;
+
+// for (i = 1; i <= length; i++){
+//     console.log(`${num} * ${i} is = ${num * i}`);
+// }
+
+// Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and
+// report that to the screen (e.g. "2 is even").
+
+// for (i = 0; i <= 20; i++){
+//     if (i % 2 === 0) {
+//         console.log(`${i} is even`);
+//     } else {
+//         console.log(`${i} is odd`);
+//     }
+// }
+// generate the even odd backword forward series
+// let counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// for (const numbers of counting) {
+//     console.log(numbers);
+// }
+// let reverse = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+//  for (const counting of reverse) {
+//      console.log(counting);
+// }
+// for (let i = 0; i <= 20; i++){
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+// for (let i = 1; i <= 19; i++){
+//     if (i % 2 !== 0) {
+//         console.log(i)
+//     }
+// }
+// for (let i = 2; i <= 20; i++){
+//     if (i % 2 === 0) {
+//         console.log(`${i}k`)
+//     }
+// }
+// have to check again that how you can find ans of 105
+// q18 ;Write a program to calculate the product of the odd integers from 1 to 7
+// let product = 1;
+// for (let i = 1; i <= 7; i++){
+//     if (i % 2 !== 0) {
+//        let multiply = product*i
+//         console.log(`the product of odd integer from 1 to 7  ${product*i} is ${multiply}`)
+//     }
+// }
+
+// loop remaing questions q19 / 20/18/16/15/14/11/10/9
+// question 9 ;
+// . Write a program to identify the largest number in the given array
+// A = [24, 53, 78, 91, 12]
