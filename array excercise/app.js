5ffc374f9c217a774d16e4a155e1fa37059a6986
usernmae <afshaanqasim07@gmail.com>
2024-08-29 05:36:49 -0700

    some questions

---
+++ array excercise/app.js
@@ -0,0 +1,60 @@
+// array excercise
+
+
+// q1: Declare an empty array using JS literal notation to store student names in future.
+// solution
+// let students = []
+// console.log(students)
+
+
+    // question 2
+    // Declare and initialize a strings array.
+// let fruits = ['apple', 'banana', 'mango', 'kiwi']
+// console.log(fruits);
+// question 3
+//    Declare and initialize a numbers array
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(numbers);
+// question 4
+// Declare and initialize a boolean array
+// let boolarry = [true, false, true, false]
+// console.log(boolarry);
+// question 5
+// Declare and initialize a mixed array.
+// let mixedarr = ["hello", 1, 2, true, "bye"]
+// console.log(mixedarr);
+// questions 6
+// Declare and Initialize an array and store available mobile networks in Pakistan.
+// let networks = ['zong', 'jazz', 'ufone', 'telenor']
+// console.log(networks);
+// questions 7
+// Declare and Initialize an array and store available education
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser
+// let education = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"]
+// for (i = 1; i < education.length; i++){
+//     console.log(`${i} : ${education[i]}`);
+// }
+// question no 08:
+// Declare and initialize an empty array to store top movies of
+// 2015. Add movies one by one in an array. Display the elements
+// & length of array in your browser. (Use array’s length method)
+let movies = ['Age of electron', 'kissingboth', 'insideout', 'anything but you']
+for (i = 0; i < movies.length; i++){
+    console.log(`${i} : ${movies[i]}`);
+    console.log(`the length of movies is ${movies.length}`)
+}
+
+// question no 09:
+// Declare and Initialize an array with your favorite cars. Show
+// a. First index of the array
+// b. Car at first index of the array
+// c.Last index of the array
+// d. Car at last index of the array
+let favcars = ['AUDI', 'VOLVO', 'FORD', 'LAMBORGHINI']
+console.log(favcars.toLocaleString());
+// a
+console.log(`First index of array is 0`)
+console.log(`Car at first index is ${favcars[0]}`)
+console.log(`Last index of array is 3`)
+console.log(`Car at last index is ${favcars[3]}`)
+
