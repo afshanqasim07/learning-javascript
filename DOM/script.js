e5f5fd92de67746e554c312a835406f644833a5e
usernmae <afshaanqasim07@gmail.com>
2024-09-03 09:54:54 -0700

    started

--- DOM/script.js
+++ DOM/script.js
@@ -1,12 +1,4 @@
 // DOM Manuplation
 
-let heading = document.querySelector("h1")
-heading.innerHTML = 'HELLO JAVASCRIPT'
-
-heading.style.backgroundColor = "lightpink";
-let para = document.querySelector("#para")
-let btn = document.getElementById("btn")
-function changethepara() {
-    para.innerHTML = "changed the html"
-}
-
+let heading = document.getElementById("heading")
+heading.innerHTML= 'HELLO JAVASCRIPT'
