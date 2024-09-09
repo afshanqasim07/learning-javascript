7bcac607cf1a475d03430004b04a643db082afa5
usernmae <afshaanqasim07@gmail.com>
2024-09-09 05:39:00 -0700

    done events

--- events/app.js
+++ events/app.js
@@ -3,15 +3,23 @@
 //     alert("image3 is clicked")
 //     console.log("image 3 is clicked");
 // }, false)
-document.getElementById("myul").addEventListener("click", function (e) {
-    console.log("ul is also clicked");
-}, true)
+// document.getElementById("myul").addEventListener("click", function (e) {
+//     console.log("ul is also clicked");
+// }, true)
 
 // it always bubles so it works like downward to upward and its false thatswhy bubling happend  in this so for egnoring it we can chnae the default value of false into true so it wil work correctly and that called capturing mood .
 //  now we have some methods to access the things in this so
 // e.stoppropagation : so it will stop bubling capturing by default
 
-document.getElementById("image").addEventListener("click", function (e) {
-    console.log(e);
-    console.log(e.target)
+// events methods
+
+
+let image = document.getElementById("image")
+image.addEventListener("click", function (e) {
+    // console.log(e)
+    // console.log(e.target)
+    // console.log(e.target.parentNode)
+    const removeit = e.target.parentNode
+    removeit.remove()
+   
 })
