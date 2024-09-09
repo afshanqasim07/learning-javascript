23ef0e9eb17c17f733a4522b8e763706c2b72f41
usernmae <afshaanqasim07@gmail.com>
2024-09-09 05:14:16 -0700

    EVENTS DONE

--- DOM Manipulation/app.js
+++ DOM Manipulation/app.js
@@ -1,17 +1,34 @@
 //  DOM MANIPULATION
-let heading = document.querySelector("#heading")
-heading.innerHTML = 'HELLO JAVASCRIPT!'
-heading.style.backgroundColor = "lightpink";
-let para = document.getElementById("para")
-let btn = document.getElementById("btn")
-function changeme() {
-    para.innerHTML = 'changed by javascript'
+// let heading = document.querySelector("#heading")
+// heading.innerHTML = 'HELLO JAVASCRIPT!'
+// heading.style.backgroundColor = "lightpink";
+// let para = document.getElementById("para")
+// let btn = document.getElementById("btn")
+// function changeme() {
+//     para.innerHTML = 'changed by javascript'
+// }
+// btn.addEventListener("click", changeme);
+
+// let myul = document.querySelectorAll("#ul")
+// Array.from(myul).forEach((item) => {
+//     item.style.backgroundColor = "lightblue"
+// })
+// DOM HOW TO MAKE HTML ELEMENENTS BY DOM
+
+// let div = document.createElement("div")
+// console.log(div);
+// div.className = 'divone'
+// div.id = 'div1'
+// div.innerText = "element by js"
+// document.body.appendChild(div)
+
+// edit nd delete by Dom in js
+// we add things by js 
+
+// addmoviesname(prompt("enter your favourite movie"))
+function adddrinks() {
+    let li = document.createElement("li")
+    li.appendChild(document.createTextNode(adddrinks))
+  document.getElementById("DRINKS").appendChild(li)
 }
-btn.addEventListener("click", changeme);
-
-let myul = document.querySelectorAll("#ul")
-Array.from(myul).forEach((item) => {
-    item.style.backgroundColor = "lightblue"
-})
-
-
+adddrinks("tea")
---
+++ events/app.js
@@ -0,0 +1,17 @@
+// events in js 
+// document.getElementById("image3").addEventListener("click", function (e) {
+//     alert("image3 is clicked")
+//     console.log("image 3 is clicked");
+// }, false)
+document.getElementById("myul").addEventListener("click", function (e) {
+    console.log("ul is also clicked");
+}, true)
+
+// it always bubles so it works like downward to upward and its false thatswhy bubling happend  in this so for egnoring it we can chnae the default value of false into true so it wil work correctly and that called capturing mood .
+//  now we have some methods to access the things in this so
+// e.stoppropagation : so it will stop bubling capturing by default
+
+document.getElementById("image").addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target)
+})
