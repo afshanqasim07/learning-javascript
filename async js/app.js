cc1a8823b76f2c48f0b6e975064a1fd5b1016231
usernmae <afshaanqasim07@gmail.com>
2024-09-10 07:35:12 -0700

    async js

---
+++ async js/app.js
@@ -0,0 +1,25 @@
+// async javascript
+//  settimeout
+//   setinterval
+//  promisses
+//  clearinterval
+//  clearinterval
+//
+//
+//
+//
+
+// set time out
+// perform function one time
+// let settimeout = setTimeout(function () {
+//     alert("hello welcome back")
+// }, 5000)
+// clear time out
+// stop the function
+// clearTimeout(settimeout)
+// set interval
+// repeat the process in between of the time you gave .
+//  const interval = setInterval(() => {
+//     console.log('HELLO WORLD!')
+// }, 3000);
+// clearInterval(interval)
