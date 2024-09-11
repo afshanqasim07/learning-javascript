2b52bbeef2499a6f2eb7f246f4ac02e3ed685944
usernmae <afshaanqasim07@gmail.com>
2024-09-11 09:32:45 -0700

    promisses done

--- promisses/app.js
+++ promisses/app.js
@@ -1,4 +1,42 @@
 // promisses
 
 // promisse one
-let promisone = new promise
+// we dont make promisses most of the time we just excecute it .
+// making promisses
+// promise one
+// let myPromise = new Promise((resolve, reject) => {
+//     // Simulate an asynchronous operation using setTimeout
+//     setTimeout(() => {
+//         let success = true; // Change this to false to simulate an error
+//         if (success) {
+//             resolve("Operation was successful!");
+//         } else {
+//             reject("Operation failed!");
+//         }
+//     }, 1000); // 1-second delay
+// });
+// myPromise
+// .then((res) => {
+//     console.log(res)
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
+
+// let agepromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let age = 15;
+//         if (age >= 18) {
+//     resolve("You can drive")
+//         } else {
+//             reject("sorry you are under age you cant drive")
+//  }
+//     },2000)
+// })
+// agepromise
+//     .then((res) => {
+//     console.log(res)
+//     })
+//     .catch((err)=>{
+//     console.log(err)
+// })
